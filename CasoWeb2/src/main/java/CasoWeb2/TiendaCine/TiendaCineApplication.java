14cf79223c58aeefebc5cfe83799f3b53b6b3f49
julianmiranmv09 <jmiranda00302@ufide.ac.cr>
2025-08-13 18:29:03 -0600

    Initial commit

---
+++ CasoWeb2/src/main/java/CasoWeb2/TiendaCine/TiendaCineApplication.java
@@ -0,0 +1,13 @@
+package CasoWeb2.TiendaCine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaCineApplication.class, args);
+	}
+
+}
---
+++ CasoWeb2/src/test/java/CasoWeb2/TiendaCine/TiendaCineApplicationTests.java
@@ -0,0 +1,13 @@
+package CasoWeb2.TiendaCine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
